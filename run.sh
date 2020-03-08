bb56d94f92f0d7fee424ede5f929f3cfb6acfe7e
DevanshG9 <devanshngholba@gmail.com>
2020-03-08 17:20:16 +0530

    Added run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
+curl https://install.meteor.com/ | sh
+cd semantic-segmentation-editor
+meteor npm install
+echo 'export MONGO_URL="mongodb://user:password@127.0.0.1:27017/labeltool-local?authSource=admin"' >> ~/.bashrc
+source ~/.bashrc
 meteor npm start
